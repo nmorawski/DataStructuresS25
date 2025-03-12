17772555537dbd38ab45f4bb3cc73f81b8b6e2de
Nicolas Morawski <nmorawski22@gmail.com>
2025-03-11 23:37:11 -0400

    lab 7

---
+++ labs/maps/check2.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <vector>
+#include <iostream>
+
+
+int main(int argc, char* argv[]){
+    std::vector<int> inputs;
+    for (int i = 1; i < argc; i++){
+        int num = std::atoi(argv[i]);
+        inputs.push_back(num);
+    }
+    std::map<int, int> modeCount;
+    int inputs_size = inputs.size();
+ 
+    for(int i = 0; i < inputs_size; ++i){
+        if (modeCount.find(inputs[i]) == modeCount.end()){
+            modeCount.insert(std::pair<int,int>(inputs[i],1));
+        } else {
+            std::map<int,int>::iterator itr = modeCount.find(inputs[i]);
+            itr->second++;
+        }
+    }
+
+    unsigned int max_mode_value = 0;
+
+    for (std::map<int, int>::const_iterator itr = modeCount.begin(); itr != modeCount.end(); itr++){
+        unsigned int map_value = itr->second;
+        if (map_value > max_mode_value){
+            max_mode_value = itr->second;
+        }
+    }
+    std::cout << "Modes: ";
+    for (std::map<int, int>::const_iterator itr = modeCount.begin(); itr != modeCount.end(); itr++){
+        unsigned int map_value = itr->second;
+        if (map_value == max_mode_value){
+            std::cout << itr->first<<std::endl;
+        }
+    }
+    return 0;
+}
--- labs/maps/phonebook.cpp
+++ labs/maps/phonebook.cpp
@@ -3,25 +3,35 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 using namespace std;
 
 // add a number, name pair to the phonebook
-void add(vector<string> &phonebook, int number, string const& name) {
+/*void add(vector<string> &phonebook, int number, string const& name) {
+  phonebook[number] = name;
+}*/
+void add(map<int, string> &phonebook, int number, string const& name) {//O(logn)
   phonebook[number] = name;
 }
 
 // given a phone number, determine who is calling
-void identify(const vector<string> & phonebook, int number) {
+/*void identify(const vector<string> & phonebook, int number) {
   if (phonebook[number] == "UNASSIGNED") 
+    cout << "unknown caller!" << endl;
+  else 
+    cout << phonebook[number] << " is calling!" << endl;
+}*/
+void identify(map<int,string> &phonebook, int number) {//O(logn)
+  if (phonebook.find(number) == phonebook.end()) 
     cout << "unknown caller!" << endl;
   else 
     cout << phonebook[number] << " is calling!" << endl;
 }
 
-
 int main() {
   // create the phonebook; initially all numbers are unassigned
-  vector<string> phonebook(10000, "UNASSIGNED");
+  //vector<string> phonebook(10000, "UNASSIGNED");
+  map<int,string> phonebook;
 
   // add several names to the phonebook
   add(phonebook, 1111, "fred");
